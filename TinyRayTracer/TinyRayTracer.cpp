a35e1107097b0ccbc7a1cb0e8d41341f2f229233
Wu Han <wuhanstudio@qq.com>
2021-08-30 17:35:55 +0100

    Step 1: write an image to the disk

--- TinyRayTracer/TinyRayTracer.cpp
+++ TinyRayTracer/TinyRayTracer.cpp
@@ -5,26 +5,32 @@
 #include <vector>
 #include "geometry.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STBI_MSC_SECURE_CRT
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#define CHANNEL_NUM 3
+
 void render() {
     const int width = 1024;
     const int height = 768;
-    std::vector<Vec3f> framebuffer(width * height);
+
+    int index = 0;
+    uint8_t* framebuffer = new uint8_t[width * height * CHANNEL_NUM];
 
     for (size_t j = 0; j < height; j++) {
         for (size_t i = 0; i < width; i++) {
-            framebuffer[i + j * width] = Vec3f(j / float(height), i / float(width), 0);
+            framebuffer[index++] = int(j / float(height) * 255);
+            framebuffer[index++] = int(i / float(width) * 255);
+            framebuffer[index++] = 0;
         }
     }
 
-    std::ofstream ofs; // save the framebuffer to file
-    ofs.open("./out.ppm");
-    ofs << "P6\n" << width << " " << height << "\n255\n";
-    for (size_t i = 0; i < height * width; ++i) {
-        for (size_t j = 0; j < 3; j++) {
-            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
-        }
-    }
-    ofs.close();
+    // You have to use 3 comp for complete jpg file. If not, the image will be grayscale or nothing.
+    stbi_write_jpg("stbjpg3.jpg", width, height, 3, framebuffer, 100);
 }
 
 int main() {
