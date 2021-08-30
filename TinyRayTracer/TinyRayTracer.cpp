8520f57ca0f9224a893b90e4c2558a903d1be8c8
Wu Han <wuhanstudio@qq.com>
2021-08-30 19:20:20 +0100

    Step 2, the crucial one: ray tracing

--- TinyRayTracer/TinyRayTracer.cpp
+++ TinyRayTracer/TinyRayTracer.cpp
@@ -1,4 +1,5 @@
-﻿#include <limits>
+﻿#define _USE_MATH_DEFINES
+#include <limits>
 #include <cmath>
 #include <iostream>
 #include <fstream>
@@ -14,18 +15,60 @@
 
 #define CHANNEL_NUM 3
 
-void render() {
+struct Sphere {
+    Vec3f center;
+    float radius;
+
+    Sphere(const Vec3f& c, const float& r) : center(c), radius(r) {}
+
+    bool ray_intersect(const Vec3f& orig, const Vec3f& dir, float& t0) const {
+        Vec3f L = center - orig;
+        float tca = L * dir;
+        float d2 = L * L - tca * tca;
+        if (d2 > radius * radius) return false;
+        float thc = sqrtf(radius * radius - d2);
+        t0 = tca - thc;
+        float t1 = tca + thc;
+        if (t0 < 0) t0 = t1;
+        if (t0 < 0) return false;
+        return true;
+    }
+};
+
+Vec3f cast_ray(const Vec3f& orig, const Vec3f& dir, const Sphere& sphere) {
+    float sphere_dist = std::numeric_limits<float>::max();
+    if (!sphere.ray_intersect(orig, dir, sphere_dist)) {
+        return Vec3f(0.2, 0.7, 0.8); // background color
+    }
+
+    return Vec3f(0.4, 0.4, 0.3);
+}
+
+void render(const Sphere& sphere) {
     const int width = 1024;
     const int height = 768;
+    const float fov = M_PI / 2.;
 
     int index = 0;
     uint8_t* framebuffer = new uint8_t[width * height * CHANNEL_NUM];
 
+    #pragma omp parallel for
     for (size_t j = 0; j < height; j++) {
         for (size_t i = 0; i < width; i++) {
-            framebuffer[index++] = int(j / float(height) * 255);
-            framebuffer[index++] = int(i / float(width) * 255);
-            framebuffer[index++] = 0;
+            // We placed the screen at the distance of 1 from the camera
+            // aspect ratio: y = 1, x = width / height
+            uint8_t distance = 1;
+            float x =   ( 2.0 * distance * (i + 0.5) / (float)width  - 1 ) * tan(fov / 2.0) * (width / (float)height);
+            float y = - ( 2.0 * distance * (j + 0.5) / (float)height - 1 ) * tan(fov / 2.0);
+            Vec3f dir = Vec3f(x, y, -1).normalize();
+            Vec3f ray = cast_ray(Vec3f(0, 0, 0), dir, sphere);
+
+            uint8_t r = int(ray.x * 255);
+            uint8_t g = int(ray.y * 255);
+            uint8_t b = int(ray.z * 255);
+            framebuffer[index++] = r;
+            framebuffer[index++] = g;
+            framebuffer[index++] = b;
         }
     }
 
@@ -34,6 +77,8 @@ void render() {
 }
 
 int main() {
-    render();
+    Sphere sphere(Vec3f(-3, 0, -16), 2);
+    render(sphere);
+
     return 0;
 }
